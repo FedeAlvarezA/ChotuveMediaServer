649d1db3ee17886e51db96dbeb4134fc34abd1bd
FMAA1997 <fede.alvarez1997@gmail.com>
2020-04-19 19:36:03 -0300

    Primer Commit, yay!

---
+++ src/app.py
@@ -0,0 +1,14 @@
+import os
+from flask import Flask, escape, request, jsonify
+from marshmallow import ValidationError
+from flask_pymongo import PyMongo
+
+app = Flask(__name__)
+app.config["DEBUG"] = True
+
+@app.route('/', methods = ['GET'])
+
+def home():
+	return '<h1>Ejemplo lala</h1>'
+
+app.run()
